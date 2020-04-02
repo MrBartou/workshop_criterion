fdfd2eb5f4ed3952214a2d17dff40d28555d0a75
MrBartou <53816747+MrBartou@users.noreply.github.com>
2020-04-02 19:58:16 +0200

    Re add all files

--- exercice 1/tests/tests_return_one.c
+++ exercice 1/tests/tests_return_one.c
@@ -1,28 +1,21 @@
 #include "my.h"
 #include <criterion/criterion.h>
-#include <criterion/redirect.h>
 
-void redirect_all_stds(void)
-{
-cr_redirect_stdout ();
-cr_redirect_stderr ();
-}
-
-Test(error_handling, return_fonction_84, .init = redirect_all_stds)
+Test(error_handling, return_fonction_84)
 {
     char *string = "error";
 
     // cr..
 }
 
-Test(error_handling, return_fonction_1, .init = redirect_all_stds)
+Test(error_handling, return_fonction_1)
 {
     char *string = "passed";
 
     // cr..
 }
 
-Test(error_handling, return_fonction_0, .init = redirect_all_stds)
+Test(error_handling, return_fonction_0)
 {
     char *string = "other";
 
--- exercice 4/src/error_handling.c
+++ exercice 4/src/error_handling.c
@@ -7,14 +7,12 @@ char *error_handling(int number)
     if (number == 0) {
         retour = "error";
         write(1, retour, 5);
-        return (retour);
     } else if (number == 1) {
         retour = "passed";
         write(1, retour, 6);
-        return (retour);
     } else {
         retour = "other";
         write(1, retour, 5);
-        return (retour);
     }
+    return (retour);
 }
